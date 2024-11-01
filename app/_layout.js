3e57138c43f5ef59bf4f8eb7dd595263554f8f05
applebarrientos <apple.barrientos@edu.sait.ca>
2024-11-01 11:21:28 -0600

    modals
    added modals and dynamic linking from lecture

--- app/_layout.js
+++ app/_layout.js
@@ -2,13 +2,14 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Stack } from "expo-router";
 
-const _layout = () => {
+const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="modal" options={{ presentation: "card" }} />
+      <Stack.Screen name="(drawer)" options={{ title: "About" }} />
     </Stack>
   );
 };
 
-export default _layout;
+export default Layout;
